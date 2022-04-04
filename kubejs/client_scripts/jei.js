f11591150811d80c02f9c5d939ed625ec2fcf8d2
TGMax233Michael <61337794+TGMax233Michael@users.noreply.github.com>
2022-04-04 13:49:08 +0800

    Add files via upload

---
+++ kubejs/client_scripts/jei.js
@@ -0,0 +1,22 @@
+onEvent('jei.hide.items', event => {
+	let ingots = ['silver']  
+	let mod = 'occultism'
+
+	 for (let i of ingots) {
+		event.hide(`${mod}:${i}_ingot`)
+
+		event.hide(`${mod}:${i}_nugget`)
+
+		event.hide(`${mod}:${i}_block`)
+
+		event.hide(`${mod}:raw_${i}`)
+
+		event.hide(`${mod}:${i}_dust`)
+
+		event.hide(`${mod}:${i}_grit`)
+			 
+ }})
+
+ onEvent('jei.hide.items', event => {
+	event.hide('occultism:raw_silver')
+  })
