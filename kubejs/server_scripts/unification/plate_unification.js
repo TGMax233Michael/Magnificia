cda671be9cb8c7b1d9eb0cc559a20023d7722da2
TGMax233Michael <61337794+TGMax233Michael@users.noreply.github.com>
2022-04-13 16:02:57 +0800

    Delete kubejs directory

--- kubejs/server_scripts/unification/plate_unification.js
+++
@@ -1,9 +0,0 @@
- onEvent('recipes', event => { 
-	let ingots = ['iron', 'gold', 'copper']   
-	let mod = 'create'
-	 
- for (let i of ingots) {
-		 
- event.replaceOutput({}, `#forge:plates/${i}`, `${mod}:${i}_sheet`)
-		 
- }})
--- kubejs/server_scripts/unification/rods_unification.js
+++
@@ -1,9 +0,0 @@
- onEvent('recipes', event => { 
-	let ingots = ['iron']   
-	let mod = 'createaddition'
-	 
- for (let i of ingots) {
-		 
- event.replaceOutput({}, `#forge:rods/${i}`, `${mod}:${i}_rod`)
-		 
- }})
