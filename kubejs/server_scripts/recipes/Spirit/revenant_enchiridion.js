2476d6ede81871ce9716930fd2b657bc570e9821
TGMax233Michael <61337794+TGMax233Michael@users.noreply.github.com>
2022-05-03 16:08:21 +0800

    Add files via upload

---
+++ kubejs/server_scripts/recipes/Spirit/revenant_enchiridion.js
@@ -0,0 +1,4 @@
+onEvent('recipes', event => {
+	event.shapeless(Item.of('patchouli:guide_book', '{"patchouli:book":"spirit:revenant_enchiridion"}'), ['minecraft:soul_sand', 'minecraft:book']).id(`kubejs:crafting/revenant_enchiridion`)
+})
+
